04d9fd3a6a4c757392c6bdc37ee6356b0d5d4ab4
jaikushwaha7 <jaikushwaha7@gmail.com>
2023-03-22 12:59:35 +0100

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-from setuptools import find_packages, setup
-from typing import list
+from setuptools import find_packages,setup
+from typing import List
 
-hyphen_e_dot '-e .'
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    This function will return the list of requirements
+    this function will return the list of requirements
     '''
-    requirements =[]
-    with open (file_path) as file_obj:
-        requirements= file_obj.readlines()
-        requirements =  [req.replace('\n','') for req in requirements]
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if hyphen_e_dot in requirements:
-            requirements.remove(hyphen_e_dot)
-
-    return requirements 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
     name = 'mlproject',
     version = '0.0.1',
     author = 'Jai',
-    author= 'jaikushwaha7@gmail.com',
+    author_email= 'jaikushwaha7@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 
